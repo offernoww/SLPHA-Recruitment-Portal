d5ddf3d636adb41a20611e541d1db24b83e3fef3
offernoww <josiahnyeti16@gmail.com>
2025-01-22 23:53:47 +0100

    Create myupcd.js

---
+++ myupcd.js
@@ -0,0 +1,7 @@
+
+
+var password = 'SLPHA PORTAL'
+password=prompt('SMS :Welcome to the SLPHA recruitment portal,Click OK to start application  ','SLPHA PORTAL');
+if (password != 'SLPHA PORTAL') {
+location.href='https://offernoww.github.io/SLPHA-Recruitment-Portal';
+}
